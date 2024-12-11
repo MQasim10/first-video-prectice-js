9daa198d121b345916d82364b6731b14e78b7c20
Qasim Haleem <qasim.icp.student@gmail.com>
2024-12-11 17:30:00 +0500

    first file of Javascript

---
+++ first.js
@@ -0,0 +1,10 @@
+console.log("hello world!")
+console.log("my name is muhammd qasim")
+
+let a = 10
+let b = 20
+let i
+for (i = 100; i > 10; i -= 10) {
+    console.log(a + b)
+}
+console.log("Hello")
