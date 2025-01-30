516eef854c254bc81e7f3d459dda8928872219c7
Qasim Haleem <qasim.icp.student@gmail.com>
2025-01-30 13:01:33 +0500

    little bit improvment

--- first.js
+++ first.js
@@ -1,10 +1,25 @@
-console.log("hello world")
-console.log("my name is muhammd qasim")
 
-let a = 10
-let b = 20
-let i
-for (i = 100; i > 10; i -= 10) {
-    console.log(a + b)
-}
-console.log("Hello")
+// var myName = "Qasim Haleem";
+// var myAge = 19;
+// console.log("Hello " + myName + " You are " + myAge + " old ");
+
+// var cmmunicationSill = 65;
+// var discreateMath = 83;
+// var OOP = 89;
+// var paStudy = 85;
+// var mgmScience = 89;
+// var total = cmmunicationSill + discreateMath + OOP + paStudy + mgmScience;
+// console.log(`you total mars in 2nd semester is  ${total}`);
+
+
+// var favouriteFood = "burger";
+// console.log(` your favourite Food is ${favouriteFood} !`);
+
+
+let a = 10;
+let b = 20;
+let temp = 0;
+temp = a;
+a = b;
+b = temp;
+console.log(a);
